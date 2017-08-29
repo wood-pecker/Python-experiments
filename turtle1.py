e005656ddd2c6999613e124b05f658597b894890
wood-pecker <mr.Pianopost@mail.ru>
2017-08-29 21:44:59 +0300

    Python turtle!

---
+++ turtle1.py
@@ -0,0 +1,10 @@
+import turtle
+t = turtle.Pen()
+t.forward(200)
+t.left(90)
+t.forward(75)
+t.left(90)
+t.forward(200)
+t.left(90)
+t.forward(75)
+t.right(270)
---
+++ turttle2.py
@@ -0,0 +1,9 @@
+import turtle
+t = turtle.Pen()
+t.left(60)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.right(120)
+t.forward(100)
+t.left(180)
